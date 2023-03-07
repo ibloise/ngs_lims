84cb5a7a8e9eefd122b91dbe0f194c2eab524f39
ibloise <ibloise62@gmail.com>
2023-03-07 13:50:18 +0100

    create setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version = '0.1.0',
     author = 'Iván Bloise Sánchez',
     description= 'Functions and other tools for Opentrons protocols',
-    packages={'ngs_lims': "ngs_lims"},
+    packages=find_packages('ngs_lims'),
     entry_points = {
     'console_scripts': [
     'lims_get_fastq=ngs_lims.get_fastq:main'
