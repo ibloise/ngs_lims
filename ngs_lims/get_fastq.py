ba806aef6fce515ab8e2894723191a11920ed31c
ibloise <ibloise62@gmail.com>
2023-03-07 13:44:54 +0100

    create main function of get_fastq

--- ngs_lims/get_fastq.py
+++ ngs_lims/get_fastq.py
@@ -4,124 +4,137 @@ import shutil
 import sys
 import datetime
 import pandas as pd
-import pymysql
-import getpass
-from sqlalchemy import create_engine
+from sql_connect import sql_connect as sql
+from constants import constants
+
+# Algoritmo temporal de obtención de archivos del datalake
 
 #ToDo:
 ## Flexibilizar para poder filtar por más metadatos
 ## Meter a log todo!!!!!! Y volcarlo en la carpeta del job
 ## Meter esto en path
-## Empaquetar las funciones
-def SQL_connect(schema, host = 'localhost', port = 3306):
-    print('Conectando MySQL')
-    print('Introduce el usuario:')
-    user = input()
-    print('Introduce la contraseña')
-    password = getpass.getpass()
-    try:
-        conexion = pymysql.connect(host=host,
-                        user=user,
-                        password=password,
-                        db= schema,
-                        port = port
-        )
-        print('Conexion establecida!')
-    except Exception as e:
-        print("No se ha podido establecer la conexión con MySQL. El proceso se ha abortado")
-        print(e)
-        exit()
-    db_data = f'mysql+pymysql://{user}:{password}@{host}:{port}/{schema}'
-    engine = create_engine(db_data, encoding='latin1')
-    cursor = conexion.cursor()
-    return (conexion, engine, cursor)
 
-parser = argparse.ArgumentParser(description="Herramienta temporal de obtención de archivos fastq del data lake")
-parser.add_argument('--sample', help = 'Se obtienen las secuencias con el número de muestre indicado. Pueden indicarse varias, separadas por , sin espacio.')
-#parser.add_argument('--csv', help='CSV con lista de muestras')
-parser.add_argument('--copy-files', help= 'Genera una copia del archivo en lugar de un enlace simbólico', action='store_true')
-parser.add_argument('--prefix', help='Prefijo para la carpeta de trabajo')
-args = parser.parse_args()
-
-sample_col = 'sample_name'
-cdate = datetime.datetime.now().strftime("%Y_%m_%d-%H-%M-%S")
-cwd = os.getcwd()
-
-schema = 'NGS_LIMS'
+def arg_parser():
+    parser = argparse.ArgumentParser(description="Herramienta temporal de obtención de archivos fastq del data lake")
+    parser.add_argument('--sample', help = 'Se obtienen las secuencias con el número de muestre indicado. Pueden indicarse varias, separadas por , sin espacio.')
+    #parser.add_argument('--csv', help='CSV con lista de muestras')
+    parser.add_argument('--copy-files', help= 'Genera una copia del archivo en lugar de un enlace simbólico', action='store_true')
+    parser.add_argument('--prefix', help='Prefijo para la carpeta de trabajo', default= '')
+    args = parser.parse_args()
+    return args
 
 # Sql connection
 
-conexion, engine, cursor = SQL_connect(schema)
-
-try:
-    datalake_bd = pd.read_sql("data_lake", engine)
-except Exception as e:
-    print('No se puede abrir la base de datos!')
-    print(e)
-    exit()
-
-if args.sample:
-    query = [str(sample).strip() for sample in args.sample.split(',')]
-
-no_paths = [sample for sample in query if sample not in pd.Series(datalake_bd[sample_col]).to_list()]
-samples = [sample for sample in query if sample not in no_paths]
-
-#Obtenemos los paths y buscamos duplicados
-
-paths_dict = {}
-paths = datalake_bd[datalake_bd[sample_col].isin(samples)]
-for sample in samples:
-    paths_dict[sample] = []
-    subset = paths[paths[sample_col] == sample].reset_index(drop=True).copy()
-    if subset.shape[0]>1:
-        breaker = True
-        while breaker:
-            breaker = False
-            print(f"La muestra {sample} tiene varios archivos")
-            for idx, row in subset.iterrows():
-                print(f"{idx} : {row['file']}")
-            print('Indica los índices seleccionados separados por comas. Ej: 1,3')
-            print('Si quieres todos los archivos, escribe all. Si no quieres ninguno, escribe any. Sal con exit')
-            input_idx = input()
-            if input_idx == 'exit':
-                sys.exit()
-            elif input_idx == 'all':
-                idxes = [idx for idx in subset.index]
-            elif input_idx == 'any':
-                idxes = []
-            else:
-                idxes = [int(n) for n in input_idx.split(',')]
-                checker = [idx for idx in idxes if idx not in subset.index]
-                if checker:
-                    print('Indice(s) incorrecto(s):')
-                    print(checker)
-                    breaker = True
-        if idxes:
-            print(subset)
-            paths_dict[sample] += [subset.loc[subset.index[idx], 'path'] for idx in idxes]
+def get_table(table_name, engine):
+    try:
+        datalake_bd = pd.read_sql(table_name, engine)
+        return datalake_bd
+    except Exception as e:
+        print('Error en la consulta de pandas')
+        print(e)
+        exit()
+    
+def create_instr(args):
+    if args.copy_files:
+        instr = shutil.copy
     else:
-        paths_dict[sample]+= [subset.loc[subset.index[0], 'path']]
+        instr = os.symlink
+    return instr
 
-# Assign instruction
-
-if args.copy_files:
-    instr = shutil.copy
-else:
-    instr = os.symlink
-
-# Create job and recover fastq
-
-job_folder = f'{cwd}/{args.prefix}-job_{cdate}'
-if not os.path.exists(job_folder):
-    os.mkdir(job_folder)
-
-for sample, files in paths_dict.items():
-    for idx, file in enumerate(files):
-        if len(files)>1:
-            instr(file, f"{job_folder}/{sample}-{idx}.fastq")
+def manage_duplicates(df, col, sample_list):
+    paths_dict = {}
+    paths = df[df[col].isin(sample_list)]
+    for sample in sample_list:
+        paths_dict[sample] = []
+        subset = paths[paths[col] == sample].reset_index(drop=True).copy()
+        if subset.shape[0]>1:
+            breaker = True
+            while breaker:
+                breaker = False
+                print(f"La muestra {sample} tiene varios archivos")
+                for idx, row in subset.iterrows():
+                    print(f"{idx} : {row['file']}")
+                print('Indica los índices seleccionados separados por comas. Ej: 1,3')
+                print('Si quieres todos los archivos, escribe all. Si no quieres ninguno, escribe any. Sal con exit')
+                input_idx = input()
+                if input_idx == 'exit':
+                    sys.exit()
+                elif input_idx == 'all':
+                    idxes = [idx for idx in subset.index]
+                elif input_idx == 'any':
+                    idxes = []
+                else:
+                    idxes = [int(n) for n in input_idx.split(',')]
+                    checker = [idx for idx in idxes if idx not in subset.index]
+                    if checker:
+                        print('Indice(s) incorrecto(s):')
+                        print(checker)
+                        breaker = True
+            if idxes:
+                print(subset)
+                paths_dict[sample] += [subset.loc[subset.index[idx], 'path'] for idx in idxes]
         else:
-            instr(file, f"{job_folder}/{sample}.fastq")
+            paths_dict[sample]+= [subset.loc[subset.index[0], 'path']]
+    return paths_dict
 
-# No paths
-if no_paths:
-    print(f'La(s) muestra(s) {" ".join(no_paths)} no se han encontrado')
+def create_job (job_tag, date, cwd, prefix = ""):
+    job_folder = f'{cwd}/{prefix}{job_tag}{date}'
+    if not os.path.exists(job_folder):
+        os.mkdir(job_folder)
+    return job_folder
+
+def main():
+    
+    #args
+    
+    args = arg_parser()
+    
+    # constants
+    cwd = os.getcwd()
+
+    sql_constants = constants.sql_constants
+
+    date_constants = constants.date_formats
+
+    prefix_constants = constants.prefix
+
+    cdate = datetime.datetime.now().strftime(date_constants.job_format)
+
+    engine = sql.SQL_connect(sql_constants.lims_schema)[1]
+
+    #instruction
+
+    instr = create_instr(args)
+
+    #get table
+
+    datalake_bd = get_table(sql_constants.data_lake_table, engine)
+    #sample list
+
+    if args.sample:
+        query = [str(sample).strip() for sample in args.sample.split(',')]
+
+    no_paths = [sample for sample in query if sample not in pd.Series(datalake_bd[sql_constants.sample_col]).to_list()]
+    samples = [sample for sample in query if sample not in no_paths]
+
+    #dupllicates
+
+    paths_dict = manage_duplicates(datalake_bd, sql_constants.sample_col, samples)
+
+
+    # Create job and recover fastq
+
+    job_folder = create_job(prefix_constants.job_prefix, cdate, cwd, args.prefix)
+
+    for sample, files in paths_dict.items():
+        for idx, file in enumerate(files):
+            if len(files)>1:
+                instr(file, f"{job_folder}/{sample}-{idx}.fastq")
+            else:
+                instr(file, f"{job_folder}/{sample}.fastq")
+    if no_paths:
+        print(f'La(s) muestra(s) {" ".join(no_paths)} no se han encontrado')
+
+
+if __name__ == "__main__":
+    main()
